8a1bc2978eefd34ac53357e7b981cfa2455b2147
Sasha <pisarevasas@yandex.ru>
2023-01-28 00:05:33 +0300

    add example005 if else

---
+++ example001_helloconsole/example002_hellouser/example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же САША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
